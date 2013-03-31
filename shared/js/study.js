9ce06a96ebfe9dac6267cf6145ff10f1ebe74ff5
sakaki <miho@trymore-inc.jp>
2013-03-31 23:27:29 +0900

    first

---
+++ shared/js/study.js
@@ -0,0 +1,9 @@
+$(function() {
+	$("#box1").click(function(){
+	$(this).css("background","rgba(0,0,0,1)");
+	});
+	
+	$(".box").hover(function(){
+		$(this).css("border","1px solid #000");
+	});
+});
